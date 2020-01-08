3229650c7a732e023bfc40b03f3c8ac90eb1ec62
modood <modood@qq.com>
2020-01-08 10:53:01 +0800

    Add support for BIP32/BIP39/BIP44

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ keygen
 
 Bitcoin Key Generator
 
+A Golang implementation of the BIP32/BIP39/BIP44 for mnemonic seeds and Hierarchical Deterministic (HD) addresses
+
+Can I trust this code?
+----------------------
+
+> Don't Trust. Verify.
+
+> We recommend every user of this library audit and verify any underlying code for its validity and suitability.
+>
+> You can do so by using this tool. https://iancoleman.io/bip39/
+
 Installation
 ------------
 
@@ -15,15 +26,19 @@ Usage
 
 ```
 Usage of keygen:
-  -c        generate a compressed public key
-  -n int    set number of keys to generate (default 20)
+  -bip39     bool   mnemonic code for generating deterministic keys (default false)
+  -pass      string protect bip39 mnemonic with a passphrase (default "")
+  -n         int    set number of keys to generate (default 20)
+  -compress  bool   generate a compressed public key (default true)
 ```
 
 Example
 -------
 
+**generate compressed public key:**
+
 ```txt
-$ keygen -c -n 20
+$ keygen
 
 Bitcoin address                    WIF(Wallet Import Format)
 --------------------------------------------------------------------------------------
@@ -48,6 +63,59 @@ Bitcoin address                    WIF(Wallet Import Format)
 1LWTkwaYgrpDgwfNT9zafJBtgiXKkuotc8 KwxcE9z1ieCE37b8UZCKph924jvYvcrFJNt4QBQe3WSjh3kdfH7h
 1FykDsmzqogch2dvXKXkrFSuT6iHapCYb2 L4QD2RzjYM1snVW54F1SnbHUormHQvNtD3GDHB59PGkn2Lv6tjmN
 
+```
+
+**generate uncompressed public key:**
+
+```txt
+$ keygen -compress=false -n 5
+
+Bitcoin address                    WIF(Wallet Import Format)
+--------------------------------------------------------------------------------------
+1C2on8GDgZjMHz4hpUUTccdbwiHmDm82qp 5KMxQBPLyfLLpAgnxnDdDgtHF323jEaqCyM7u9TgRxJvkTPSoM1
+1JymTfXZQWQqPZV7GENiPb6rAWrnYg9B8T 5Hqwm9jYdLgMZawGxFW3kdbUmu1hWV8Krjnc33vRPj1T8Zz2eVZ
+1KZKt4N1QpcR9qnyWnZvAe9ccK9SWzv2AT 5KXZFiE3Rz5s1zYym8voutZs4a4NBkz3pPVQfJhrSCeCVZfgJDU
+1C91FBVHU5t3N4oUFe7XDCVwjbcRKvSSSe 5JwKt754DN8SjustfVuuHo9wFzDWSZDirSwGbiXC3hUnopbiw4J
+1DN7rfvAYB77acrk91SmQHGWtjLh23uG5K 5K7RMVjdWRaE6PwgvGYW1jE8ydLLjnzYWFx3jBGEWekUD8pcZmC
+
+```
+
+**generate bip39 mnemonic with passphrase:**
+
+```txt
+$ keygen -bip39 -pass=123456 -n 20
+
+BIP39 Mnemonic:                     arctic weird hurry attract morning mention island laundry gasp lecture urban season
+BIP39 Passphrase:                   123456
+BIP39 Seed:                         fe69ed69eca284eaf0146a7aafedf99986cba2f8805fcf8e538bd342683086de830fecda7dde9140c48737c97467fd87df8f5c16a6f90b8af82d51cdb3180e8d
+BIP32 Root Key:                     m                xprv9s21ZrQH143K4EUqf9hLagzxnpEkGJGxFD7q7ANbMYrYrzk8gsJDGCDiVwxbiBBAiJHNN8idtUG6YTmefdJaat9WwpZG7xfbqY8VUWAipmQ
+BIP44 Account Extended Private Key: m/44'/0'/0'      xprv9yN4thyP1Py9uhiTh4Z4h4q3ed4wPr6boSugrCwyQSd2D1wCc7LFepjUx2c6JZW2WZYwKZA42vVQRQAQy9W2TpuqsEr3nWrVzaWsNYVyait
+BIP44 Account Extended Public  Key:                  xpub6CMRJDWGqmXT8Bnvo6654CmnCeuRoJpTAfqHebMaxnA15pGM9eeWCd3xoHbuBncbfxiQ1ueRBU8hHmxg3AUvJ9V4HsKfvS52HZnfi5H8g9N
+BIP32 Extended Private Key:         m/44'/0'/0'/0    xprv9zzuDNBQd7f9XAVDx7kZYdKFyQ2KYxLGYyhdFcRqZSqWNA9GqZayGSVtFj4PPGFqMyUyyP5JCYc3neuuk2y9xM87fbKseAcVr3GXz6ooCrf
+BIP32 Extended Public  Key:                          xpub6DzFcsiJTVDSjeZh49HZumFzXRroxR47vCdE3zqT7nNVExURP6uDpEpN73jgZxSoqiEJ2t6QwgRouR7UKzzAosBwxzCBkC2AqwqQqf6V42J
+
+Path               Bitcoin address                    WIF(Wallet Import Format)
+----------------------------------------------------------------------------------------------------------
+m/44'/0'/0'/0/0    1NjXHEGvoCddZqAX4gKkNpRR2vBBLSDNyR L3b8Jogy6Q3347mKNkiF8ccDTvjC6w1GQaZx386sfNQdYPUbQmW5
+m/44'/0'/0'/0/1    1AxENeqLKZ7fnprCXFWo2n9F1X8b6qahX4 KyWyJApkpoa1nzkTDKWJ6dPHVBBub7xb6cwH7eeq4keJNzrsokpv
+m/44'/0'/0'/0/2    1B5EXNUsKks1kdN2ojRh8ysJhBA2v4ZYrM KywK8FGqvjG7v7Xyo96Bw1s6LA9qUxTEafXmjWpgMKEf23mtC3nx
+m/44'/0'/0'/0/3    19xa4Ehkk1SeDRzi5Q2fv2V9x8s5oEVmeD Kx9buvsAkMmdTK3vj85Twe4n7JtxurvyfcawwR9EaYVvFrjMQrzn
+m/44'/0'/0'/0/4    1DXTaFQevfgh2TLnPawCMi9qXbek47WjpY L4oXfAWAzMSYWe5fz5hkVq1C9N7K9nE5rey3LR97ze64rJaggWpq
+m/44'/0'/0'/0/5    1DLygBKy4msVcNmRtX77FCczjCNiVhmeka L2qSH5yG5fyTEyjpTkQY1SpP2ZH1hA6oFEG6r62adVj62tsoeEps
+m/44'/0'/0'/0/6    1DvvtZuiV1pLWy8up4Bek9vo1BmW9NQdZE KyGZhM8VV1ee5J9yxV2LfGtzbEGTRMwEtq1z1FqFAC1E9oToyTjq
+m/44'/0'/0'/0/7    1AxDkG3pB4BVvvFdjr4evfPEk7W6pFE9si KzUAheye2X8ZZSnXonYtRhgRYYDmKdVXujH3U66o1XL5GJgzDvFJ
+m/44'/0'/0'/0/8    13ioixpQZktK8545LdDx9Mnyhfmnf97EFd KwGLLqCr8PGQTRBmY4eB9WioWwCcwLZTyvVjgYCHuQFuvxN28Xnd
+m/44'/0'/0'/0/9    1DAZb5GoCfweEhfKrPvQhpCrw4tRzR6ncb L3muRZdupCEToHEeiJggJxH8KXnqmqsnVEtLHQkeSvfv3WHWFGJW
+m/44'/0'/0'/0/10   1CvtHeSKJRpAySKjVpXFjwkjoW9WBRSUys L4GaeuCrroeRtL7aNXMopseT8HVohvnFtzViuddeVffSv2LrZxAL
+m/44'/0'/0'/0/11   16D9K61PJEcoQi87BNCkV6MvGCgRbFBmmy Ky8k3vBntawmZdzzdsL94TsCCmpnzVcvMGPg2HmvtL93C3im45RZ
+m/44'/0'/0'/0/12   1CVtw63QxLteXUh9HYFu7ZwXy9yfhDD2ud L49yfGJgsVL1UZEmR95JpHzwM7rEwUAbasE5dzNGvoyFStyxEUR8
+m/44'/0'/0'/0/13   1CUhG3eLmaiMDhKTJEnQsyi5mosKiN4HJS L1MGutW83uAdxR6mHv5gQzLZpMEkg544pENL3PVRsLcmq3kjZxYJ
+m/44'/0'/0'/0/14   1ASfwbiqz8VAhPbT3oyfKPe1Bq9phdstby KyoPSvTCZ9a7DuXpJysM681wQNZRgMLGwm4sTFG9u9uNovaGJw98
+m/44'/0'/0'/0/15   1AkJsCgdibnYogKzFDYLbEqYssrMi9iwfG Kyevv4ty2KFu7dCfYXoKKtoJaZngsa3vY1tf1jK1S6c1n93Yp33e
+m/44'/0'/0'/0/16   1R53m5qXCNwFLhLdZbDH8NAe7sw878Nqi  L3371CF2p3RiZN4SJJR9YbogvrRn9webuk8uniTbMTHj1URX52A2
+m/44'/0'/0'/0/17   1Lj8Y2ruA8EevVWhKzP7cQPgm7EtGefXSH L5UfKHAemFQLeerJpf9oZy3ybChSiYeTH4bJku4FHGPXeV7XKC81
+m/44'/0'/0'/0/18   17KLNxs3SoJzxPkhMrEFVBKSMNNJrzLNN7 KxyPLEtUJWkb6njR3bc4HB9EucbgyhPPDTFD3hb3gFRD8Z9UM19q
+m/44'/0'/0'/0/19   1FS7ddyAe1gBKJpk11qNjRdhGQB6RsJNUz L4Nu5DGEEh48aVRLfF8wSjnKjERQ3UyLaUXn7mdmDdqqGKBwvJYf
 ```
 
 License
--- main.go
+++ main.go
@@ -5,22 +5,310 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"sync"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
+	"github.com/tyler-smith/go-bip32"
+	"github.com/tyler-smith/go-bip39"
 )
 
+// Purpose is a constant set to 44' (or 0x8000002C) following the BIP43 recommendation.
+// It indicates that the subtree of this node is used according to this specification.
+//
+// What does 44' mean in BIP44?
+// https://bitcoin.stackexchange.com/questions/74368/what-does-44-mean-in-bip44
+//
+// 44' means that hardened keys should be used. The distinguisher for whether
+// a key a given index is hardened is that the index is greater than 2^31,
+// which is 2147483648. In hex, that is 0x80000000. That is what the apostrophe (') means.
+// The 44 comes from adding it to 2^31 to get the final hardened key index.
+// In hex, 44 is 2C, so 0x80000000 + 0x2C = 0x8000002C.
+const (
+	Apostrophe uint32 = 0x80000000 // 0'
+	Purpose    uint32 = 0x8000002C // 44'
+)
+
+// CoinType SLIP-0044 : Registered coin types for BIP-0044
+// https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+type CoinType = uint32
+
+const (
+	CoinTypeBTC CoinType = 0x80000000
+	CoinTypeLTC CoinType = 0x80000002
+	CoinTypeETH CoinType = 0x8000003c
+	CoinTypeEOS CoinType = 0x800000c2
+)
+
+type Key struct {
+	path     string
+	bip32Key *bip32.Key
+}
+
+func (k *Key) GetWIF(compress bool) (wif, address string, err error) {
+	prvKey, _ := btcec.PrivKeyFromBytes(btcec.S256(), k.bip32Key.Key)
+	return GenerateFromBytes(prvKey, compress)
+}
+
+// https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
+// bip44 define the following 5 levels in BIP32 path:
+// m / purpose' / coin_type' / account' / change / address_index
+
+func (k *Key) GetPath() string {
+	return k.path
+}
+
+type KeyManager struct {
+	mnemonic   string
+	passphrase string
+	keys       map[string]*bip32.Key
+	mux        sync.Mutex
+}
+
+func NewKeyManager(bitSize int, passphrase string) (*KeyManager, error) {
+	entropy, err := bip39.NewEntropy(bitSize)
+	if err != nil {
+		return nil, err
+	}
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		return nil, err
+	}
+
+	km := &KeyManager{
+		mnemonic:   mnemonic,
+		passphrase: passphrase,
+		keys:       make(map[string]*bip32.Key, 0),
+	}
+	return km, nil
+}
+
+func (km *KeyManager) GetMnemonic() string {
+	return km.mnemonic
+}
+
+func (km *KeyManager) GetPassphrase() string {
+	return km.passphrase
+}
+
+func (km *KeyManager) GetSeed() []byte {
+	return bip39.NewSeed(km.GetMnemonic(), km.GetPassphrase())
+}
+
+func (km *KeyManager) getKey(path string) (*bip32.Key, bool) {
+	km.mux.Lock()
+	defer km.mux.Unlock()
+
+	key, ok := km.keys[path]
+	return key, ok
+}
+
+func (km *KeyManager) setKey(path string, key *bip32.Key) {
+	km.mux.Lock()
+	defer km.mux.Unlock()
+
+	km.keys[path] = key
+}
+
+func (km *KeyManager) GetMasterKey() (*bip32.Key, error) {
+	path := "m"
+
+	key, ok := km.getKey(path)
+	if ok {
+		return key, nil
+	}
+
+	key, err := bip32.NewMasterKey(km.GetSeed())
+	if err != nil {
+		return nil, err
+	}
+
+	km.setKey(path, key)
+
+	return key, nil
+}
+
+func (km *KeyManager) GetPurposeKey() (*bip32.Key, error) {
+	path := fmt.Sprintf(`m/%d'`, Purpose-Apostrophe)
+
+	key, ok := km.getKey(path)
+	if ok {
+		return key, nil
+	}
+
+	parent, err := km.GetMasterKey()
+	if err != nil {
+		return nil, err
+	}
+
+	key, err = parent.NewChildKey(Purpose)
+	if err != nil {
+		return nil, err
+	}
+
+	km.setKey(path, key)
+
+	return key, nil
+}
+
+func (km *KeyManager) GetCoinTypeKey(coinType CoinType) (*bip32.Key, error) {
+	path := fmt.Sprintf(`m/%d'/%d'`, Purpose-Apostrophe, coinType-Apostrophe)
+
+	key, ok := km.getKey(path)
+	if ok {
+		return key, nil
+	}
+
+	parent, err := km.GetPurposeKey()
+	if err != nil {
+		return nil, err
+	}
+
+	key, err = parent.NewChildKey(coinType)
+	if err != nil {
+		return nil, err
+	}
+
+	km.setKey(path, key)
+
+	return key, nil
+}
+
+func (km *KeyManager) GetAccountKey(coinType CoinType, account uint32) (*bip32.Key, error) {
+	path := fmt.Sprintf(`m/%d'/%d'/%d'`, Purpose-Apostrophe, coinType-Apostrophe, account)
+
+	key, ok := km.getKey(path)
+	if ok {
+		return key, nil
+	}
+
+	parent, err := km.GetCoinTypeKey(coinType)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err = parent.NewChildKey(account + Apostrophe)
+	if err != nil {
+		return nil, err
+	}
+
+	km.setKey(path, key)
+
+	return key, nil
+}
+
+// GetChangeKey ...
+// https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#change
+// change constant 0 is used for external chain
+// change constant 1 is used for internal chain (also known as change addresses)
+func (km *KeyManager) GetChangeKey(coinType CoinType, account, change uint32) (*bip32.Key, error) {
+	path := fmt.Sprintf(`m/%d'/%d'/%d'/%d`, Purpose-Apostrophe, coinType-Apostrophe, account, change)
+
+	key, ok := km.getKey(path)
+	if ok {
+		return key, nil
+	}
+
+	parent, err := km.GetAccountKey(coinType, account)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err = parent.NewChildKey(change)
+	if err != nil {
+		return nil, err
+	}
+
+	km.setKey(path, key)
+
+	return key, nil
+}
+
+func (km *KeyManager) GetKey(coinType CoinType, account, change, index uint32) (*Key, error) {
+	path := fmt.Sprintf(`m/%d'/%d'/%d'/%d/%d`, Purpose-Apostrophe, coinType-Apostrophe, account, change, index)
+
+	key, ok := km.getKey(path)
+	if ok {
+		return &Key{path: path, bip32Key: key}, nil
+	}
+
+	parent, err := km.GetChangeKey(coinType, account, change)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err = parent.NewChildKey(index)
+	if err != nil {
+		return nil, err
+	}
+
+	km.setKey(path, key)
+
+	return &Key{path: path, bip32Key: key}, nil
+}
+
 func main() {
-	compress := flag.Bool("c", false, "generate a compressed public key")
+	compress := flag.Bool("compress", true, "generate a compressed public key")
+	bip39 := flag.Bool("bip39", false, "mnemonic code for generating deterministic keys")
+	pass := flag.String("pass", "", "protect bip39 mnemonic with a passphrase")
 	number := flag.Int("n", 20, "set number of keys to generate")
+
 	flag.Parse()
+
+	if *bip39 {
+		km, err := NewKeyManager(128, *pass)
+		if err != nil {
+			log.Fatal(err)
+		}
+		masterKey, err := km.GetMasterKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+		accountKey, err := km.GetAccountKey(CoinTypeBTC, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		changeKey, err := km.GetChangeKey(CoinTypeBTC, 0, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		passphrase := km.GetPassphrase()
+		if passphrase == "" {
+			passphrase = "<none>"
+		}
+		fmt.Printf("\n%-35s %s\n", "BIP39 Mnemonic:", km.GetMnemonic())
+		fmt.Printf("%-35s %s\n", "BIP39 Passphrase:", passphrase)
+		fmt.Printf("%-35s %x\n", "BIP39 Seed:", km.GetSeed())
+		fmt.Printf("%-35s %-16s %s\n", "BIP32 Root Key:", "m", masterKey.B58Serialize())
+		fmt.Printf("%-35s %-16s %s\n", "BIP44 Account Extended Private Key:", "m/44'/0'/0'", accountKey.B58Serialize())
+		fmt.Printf("%-35s %-16s %s\n", "BIP44 Account Extended Public  Key:", "", accountKey.PublicKey().B58Serialize())
+		fmt.Printf("%-35s %-16s %s\n", "BIP32 Extended Private Key:", "m/44'/0'/0'/0", changeKey.B58Serialize())
+		fmt.Printf("%-35s %-16s %s\n", "BIP32 Extended Public  Key:", "", changeKey.PublicKey().B58Serialize())
+
+		fmt.Printf("\n%-18s %-34s %-s\n", "Path", "Bitcoin address", "WIF(Wallet Import Format)")
+		fmt.Println(strings.Repeat("-", 106))
+		for i := 0; i < *number; i++ {
+			key, err := km.GetKey(CoinTypeBTC, 0, 0, uint32(i))
+			if err != nil {
+				log.Fatal(err)
+			}
+			wif, address, err := key.GetWIF(*compress)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%-18s %-34s %s\n", key.GetPath(), address, wif)
+		}
+		fmt.Println()
+		return
+	}
 
 	fmt.Printf("\n%-34s %-s\n", "Bitcoin address", "WIF(Wallet Import Format)")
 	fmt.Println(strings.Repeat("-", 86))
 
 	for i := 0; i < *number; i++ {
-		wif, addr, err := generate(*compress)
+		wif, addr, err := Generate(*compress)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -29,12 +317,15 @@ func main() {
 	fmt.Println()
 }
 
-func generate(compress bool) (wif, addr string, err error) {
+func Generate(compress bool) (wif, addr string, err error) {
 	prvKey, err := btcec.NewPrivateKey(btcec.S256())
 	if err != nil {
 		return "", "", err
 	}
+	return GenerateFromBytes(prvKey, compress)
+}
 
+func GenerateFromBytes(prvKey *btcec.PrivateKey, compress bool) (wif, addr string, err error) {
 	btcwif, err := btcutil.NewWIF(prvKey, &chaincfg.MainNetParams, compress)
 	if err != nil {
 		return "", "", err
